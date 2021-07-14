d2da3b5d4589669db9471e894e5646f8178d0d8b
alexiuadrian <adi.alexiu@gmail.com>
2021-07-14 11:59:33 +0300

    Modified Program.cs

--- Editoru/Program.cs
+++ Editoru/Program.cs
@@ -17,6 +17,10 @@ namespace Editoru
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+            
         }
     }
 }
